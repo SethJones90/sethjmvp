5e4fff5885506bd5ecdfd9fd5512bd778e671eb0
Seth <seth@seths-mbp.attlocal.net>
2019-03-03 11:37:40 -0600

    added form to get in touch and copy to why me

--- client/src/components/GetInTouch.js
+++ client/src/components/GetInTouch.js
@@ -5,7 +5,13 @@ class GetInTouch extends Component {
   render() {
     return (
     <div>
-      <h1>Get In Touch</h1>
+      <h1>GET IN TOUCH</h1>
+      <form>
+        <input name="name" type="text" className="getInTouchInput"/>
+        <input name="email" type="text" className="getInTouchInput"/>
+        <input name="message" type="text" className="getInTouchInput"/>  
+        <input type="submit" value="submit" className="getInTouchInput"/>
+      </form>
     </div>
     )
   }
--- client/src/components/WhyMe.js
+++ client/src/components/WhyMe.js
@@ -5,7 +5,80 @@ class WhyMe extends Component {
   render() {
     return (
     <div>
-      <h1>Why Me?</h1>
+      <h1>WHY ME?</h1>
+      <div>
+
+        Are you an Entrepreneur, trying desperately to get an MVP off the ground?
+
+        Have you spent countless hours googling agencies, or browsing profiles on Upwork/Elance just to find that they eat up countless billable hours, and deliver, for lack of better words, shit?
+
+        If so, I’m about to be your best friend.
+
+        I’m Seth Jones, an MVP consultant and entrepreneur who has spent the last 8 years working in the tech space, honing my skills in web development and business after a really undesirable experience, working with countless design and development teams, spending thousands of dollars with very little or nothing to show for it.
+
+        After going through this painful process, I decided to learn to code, so I wouldn’t get ripped off ever again - so I could help others just like me.
+
+        While working in tech, I’ve learned that speed and execution are key, and if you falter, you’re as good as obsolete.
+
+        Unfortunately I’ve also learned what it’s like to just want to bring an idea into the world, and to have limited funds, where every dollar matters, and nothing gets your blood boiling like spending it without a result.
+
+        That’s why I decided to start consulting - so you don’t have to go through what I did, and spend the next decade learning how to code.
+
+        Forget the brochures, forget the technical jargon. You can browse that later. For all intents and purposes, what I do is make things happen for you.
+
+        Why choose me?
+
+        I get you results no matter where you are in your mvp process.
+
+        I understand business.
+
+        I have a broad knowledge of product development, from design to development, positioning, scalability, and support.
+
+        I know you don’t care about the technical specs and jargon, so I won’t bore you with them. 
+
+        I deliver.
+
+        Your dream isn’t too crazy or too big.
+
+        Most importantly, I have been EXACTLY where you are.
+
+        So what can I do for you?
+
+        I can get your web app from 0 to MVP, no matter where you are in the process. Period. Point blank. No bullshit. If you sign with me, you can stand confident that your MVP is as good as delivered.
+
+        Milestones: 
+
+        Within 24 hours, a call/meeting putting together your MVP’s flight plan.
+
+        Within 72 hours, a first glance at your baby/progress.
+
+        Within weeks/months (depending on the complexity of your idea), your MVP complete. We will come up with a realistic timeline to suit your needs.
+
+        Price:
+
+        $100 consultation (1hr) - skip buying the nikes, and get the flight plan for the idea that could potentially change the world, and your life. This call alone is worth its weight in gold, trust me.
+
+        +$400 for first glance - this is meant to be token of good faith. 
+
+        You will see the first step our your action plan completed.
+
+        If you’re starting with nothing, your baby being tangible for the first time. You will see this within 72 hours of setting up your action plan. **it will be rough/half broken/slightly ugly, but that’s how development works.**
+
+        (8 hrs of work **personal calculation**)
+
+        $500 = consultation and first glance 
+
+        ==============================
+
+        (Remainder of MVP cost discussed on initial call)
+
+        If you have questions regarding pricing, email me here. We’ll figure it out.
+
+        To schedule a consultation, here’s my calendar (maybe have billing/questionnaire)
+
+        So what are you waiting for? Seriously guys, I would have killed for this when I was starting.
+
+      </div>
     </div>
     )
   }
